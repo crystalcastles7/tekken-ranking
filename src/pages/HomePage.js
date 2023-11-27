095b32d62ee9bf603a3b9a0ad81e29f54a0051c7
crystalcastles7 <asilaygun24@gmail.com>
2023-11-28 00:59:11 +0300

    update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,42 +5,86 @@ import { connect, useDispatch, useSelector } from 'react-redux';
 function HomePage(props) {
 
   const players = useSelector((state) => state.players);
+  const isAdmin = localStorage.getItem("username") === "crystalcastlesss"
+
+  const addNewPlayer = async () => {
+    alert("adding new player.... ")
+    let playerObject = {
+      name: "SomeUsername",
+      winCount: 0,
+      loseCount: 0,
+      mainCharacterId: 40,
+      classValue: 9,
+      lastPlayed: "-",
+      playedCharaterIds: []
+    }
+
+    fetch(`${process.env.REACT_APP_API_URL}/add-player`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ playerObject })
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Response data:', data);
+        alert("hopefully added")
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
 
   return (
 
-    <table>
-      <thead>
-        <tr>
-          <th>Rank</th>
-          <th>Class</th>
-          <th>Name</th>
-          <th>Record</th>
-          <th>Character</th>
-          {/* <th>Rank</th> */}
-          <th>Last Played</th>
-        </tr>
-      </thead>
-      <tbody>
+    <div>
+      {isAdmin &&
+        <div className="add-btn">
+          <button onClick={addNewPlayer}>Add new player!</button>
+        </div>
+      }
 
-        {players && players.players.length > 0 && players.players.map(((player, index) => {
-          return (
-            <tr key={index}>
-              <td>#{index+1}</td>
-              <td>{player.class}</td>
-              <td>{player.name}</td>
-              <td>{player.win} - {player.lose}</td>
-              <td className="main-char-container">
-                <img className="char-img" src={"../../tekken-ranking/images/characters/" + player.mainCharacterId + ".png"} />
-              </td>
-              {/* <td>{player.rank}</td> */}
-              <td>{player.lastPlayed}</td>
-            </tr>
-          )
-        }))
+      <table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Class</th>
+            <th>Name</th>
+            <th>Record</th>
+            <th>Character</th>
+            {/* <th>Rank</th> */}
+            <th>Last Played</th>
+          </tr>
+        </thead>
+        <tbody>
 
-        }
-      </tbody>
-    </table>
+          {players && players.players.length > 0 && players.players.map(((player, index) => {
+            return (
+              <tr key={index}>
+                <td>#{index + 1}</td>
+                <td>{player.class}</td>
+                <td>{player.name}</td>
+                <td>{player.win} - {player.lose}</td>
+                <td className="main-char-container">
+                  <img className="char-img" src={"../../tekken-ranking/images/characters/" + player.mainCharacterId + ".png"} />
+                </td>
+                {/* <td>{player.rank}</td> */}
+                <td>{player.lastPlayed}</td>
+              </tr>
+            )
+          }))
+
+          }
+        </tbody>
+      </table>
+
+    </div>
 
   );
 
