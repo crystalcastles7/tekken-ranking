b4266337bcea8d8ee00a3093a7bbe088fad268e2
crystalcastles7 <asilaygun24@gmail.com>
2023-11-28 01:53:52 +0300

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,76 +7,31 @@ import { setCharactersList } from '../actions/characters';
 import { Link } from 'react-router-dom';
 function Header() {
 
+  const fetchAndSetPlayers = async () => {
+
+    const fetchUrl = `${process.env.REACT_APP_API_URL}/get-all-players`
+    console.log('Fetching: ' + fetchUrl);
+    fetch(fetchUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('fetched data: ' + data);
+        if (data) dispatch(setPlayersList(data.playersList));
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+
   const dispatch = useDispatch();
 
   useEffect(() => {
 
-    const playersList = [
-      { name: "RavenHaze", win: 0, lose: 0, mainCharacterId: 46, rank: "Tekken God Omega", class: "S+", lastPlayed: "-" },
-      { name: "Big Boss", win: 0, lose: 0, mainCharacterId: 21, rank: "Tekken God Omega", class: "S+", lastPlayed: "-" },
-      { name: "crystalcastlesss", win: 1, lose: 0, mainCharacterId: 42, rank: "Tekken God", class: "S", lastPlayed: "26 Kasim 2023" },
-      { name: "Eternal", win: 0, lose: 0, mainCharacterId: 18, rank: "Tekken God Omega", class: "S", lastPlayed: "-" },
-      { name: "Kicking Machine", win: 0, lose: 0, mainCharacterId: 19, rank: "Tekken God", class: "S", lastPlayed: "-" },
-      { name: "ilayakreb", win: 0, lose: 0, mainCharacterId: 31, rank: "Tekken God", class: "S", lastPlayed: "-" },
-      { name: "Naustrazer", win: 0, lose: 0, mainCharacterId: 8, rank: "Tekken God", class: "A", lastPlayed: "-" },
-      { name: "Emrah-Kjl", win: 0, lose: 0, mainCharacterId: 26, rank: "Tekken God", class: "A", lastPlayed: "-" },
-      { name: "LahmacunKappa", win: 0, lose: 0, mainCharacterId: 47, rank: "Yaksa", class: "A", lastPlayed: "-" },
-      { name: "Dalzker", win: 1, lose: 0, mainCharacterId: 21, rank: "Tekken King", class: "A", lastPlayed: "26 Kasim 2023" },
-      { name: "MishimaBay", win: 0, lose: 0, mainCharacterId: 26, rank: "Emperor", class: "A", lastPlayed: "-" },
-      { name: "Egecan", win: 0, lose: 0, mainCharacterId: 25, rank: "Emperor", class: "A", lastPlayed: "-" },
-      { name: "TheRevivalWisdom", win: 0, lose: 0, mainCharacterId: 50, rank: "Emperor", class: "A", lastPlayed: "-" },
-      { name: "UTQ", win: 0, lose: 0, mainCharacterId: 36, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "King Kao", win: 0, lose: 0, mainCharacterId: 21, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "KaTkOoT", win: 0, lose: 0, mainCharacterId: 26, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "musicianofall", win: 0, lose: 0, mainCharacterId: 19, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "Baris", win: 0, lose: 0, mainCharacterId: 7, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "HasanPamuk", win: 0, lose: 0, mainCharacterId: 32, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "Turkishxiayou", win: 0, lose: 0, mainCharacterId: 48, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "Unsweetdream", win: 0, lose: 1, mainCharacterId: 11, rank: "Tekken God", class: "B", lastPlayed: "26 Kasim 2023" },
-      { name: "Flark", win: 0, lose: 0, mainCharacterId: 47, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "Aeolo", win: 0, lose: 0, mainCharacterId: 48, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "Züzü", win: 0, lose: 0, mainCharacterId: 47, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "Morde93", win: 0, lose: 0, mainCharacterId: 41, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "ChaosWarrior", win: 0, lose: 1, mainCharacterId: 5, rank: "Tekken God", class: "B", lastPlayed: "26 Kasim 2023" },
-      { name: "LixeraMe", win: 0, lose: 0, mainCharacterId: 27, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "Neoratt", win: 0, lose: 0, mainCharacterId: 27, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "ByTury", win: 0, lose: 0, mainCharacterId: 7, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "dodekaman", win: 0, lose: 0, mainCharacterId: 51, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "homicidal ocean panda", win: 0, lose: 0, mainCharacterId: 4, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "volkan_yildiz_tr", win: 0, lose: 0, mainCharacterId: 42, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "stremagod", win: 0, lose: 0, mainCharacterId: 34, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "NoPain_TR", win: 0, lose: 0, mainCharacterId: 45, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "Züzü", win: 0, lose: 0, mainCharacterId: 47, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "Qwerly", win: 0, lose: 0, mainCharacterId: 27, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "Ersin_1910", win: 0, lose: 0, mainCharacterId: 27, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "Astroth_tr", win: 0, lose: 0, mainCharacterId: 18, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "İzzetju", win: 0, lose: 0, mainCharacterId: 7, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "Göksal", win: 0, lose: 0, mainCharacterId: 3, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "Falcon", win: 0, lose: 0, mainCharacterId: 29, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "ViruSick", win: 0, lose: 0, mainCharacterId: 27, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "Breaky", win: 0, lose: 0, mainCharacterId: 4, rank: "Tekken God", class: "B", lastPlayed: "-" },
-      { name: "A06Z", win: 0, lose: 0, mainCharacterId: 34, rank: "Tekken God", class: "C", lastPlayed: "-" },
-      { name: "OkumuşPaul", win: 0, lose: 0, mainCharacterId: 45, rank: "Tekken God", class: "C", lastPlayed: "-" },
-      { name: "Atunn", win: 0, lose: 0, mainCharacterId: 18, rank: "Tekken God", class: "C", lastPlayed: "-" },
-      { name: "YasOnline", win: 0, lose: 0, mainCharacterId: 5, rank: "Tekken God", class: "C", lastPlayed: "-" },
-      { name: "ByAbu", win: 0, lose: 0, mainCharacterId: 9, rank: "Tekken God", class: "C", lastPlayed: "-" },
-      { name: "By_Gofrettin", win: 0, lose: 0, mainCharacterId: 29, rank: "Tekken God", class: "C", lastPlayed: "-" },
-      { name: "Xenacial", win: 0, lose: 0, mainCharacterId: 14, rank: "Tekken God", class: "C", lastPlayed: "-" },
-      { name: "Alecia", win: 0, lose: 0, mainCharacterId: 9, rank: "Tekken God", class: "C", lastPlayed: "-" },
-      { name: "MetinHwo", win: 0, lose: 0, mainCharacterId: 19, rank: "Tekken God", class: "C", lastPlayed: "-" },
-      { name: "Yachiru662", win: 0, lose: 0, mainCharacterId: 29, rank: "Tekken God", class: "C", lastPlayed: "-" },
-      { name: "Mokujin", win: 0, lose: 0, mainCharacterId: 1, rank: "Tekken God", class: "C", lastPlayed: "-" },
-      { name: "Prometisycl", win: 0, lose: 0, mainCharacterId: 21, rank: "Tekken God", class: "C", lastPlayed: "-" },
-      { name: "TariKazama", win: 0, lose: 0, mainCharacterId: 48, rank: "Tekken God", class: "C", lastPlayed: "-" },
-      { name: "SoulActive", win: 0, lose: 0, mainCharacterId: 18, rank: "Tekken God", class: "C", lastPlayed: "-" },
-      { name: "Burak_can", win: 0, lose: 0, mainCharacterId: 29, rank: "Tekken God", class: "C", lastPlayed: "-" },
-      { name: "CENK_Games", win: 0, lose: 0, mainCharacterId: 45, rank: "Tekken God", class: "C", lastPlayed: "-" },
-      { name: "LoCo54", win: 0, lose: 0, mainCharacterId: 38, rank: "Tekken God", class: "C", lastPlayed: "-" },
-      { name: "Killerfrostmehmet", win: 0, lose: 0, mainCharacterId: 36, rank: "Tekken God", class: "C", lastPlayed: "-" },
-      { name: "Hopdediks", win: 0, lose: 0, mainCharacterId: 17, rank: "Tekken God", class: "C", lastPlayed: "-" }
-    ];
-
-    dispatch(setPlayersList(playersList));
+    fetchAndSetPlayers();
 
     /* matches */
     const matchesList = [
