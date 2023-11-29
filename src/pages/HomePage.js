3a7012d5ac3555926d7c88b19a6b4e8a85df969e
crystalcastles7 <asilaygun24@gmail.com>
2023-11-29 20:30:24 +0300

    update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -85,7 +85,7 @@ function HomePage(props) {
                   <td className="main-char-container">
                     <img className="char-img" src={"../../tekken-ranking/images/characters/" + player.mainCharacterId + ".png"} />
                   </td>
-                  <td>{player.points} - {player.points}</td>
+                  <td>{player.points}</td>
                   {/* <td>{player.rank}</td> */}
                   {/* <td>{player.lastPlayed}</td> */}
                 </tr>
